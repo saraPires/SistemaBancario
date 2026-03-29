65a8d026f4acc6423881e11ba7d319a2cc608821
sarapiresdev <sararegina.pires@gmail.com>
2026-03-29 00:15:55 -0300

    Projeto Sistema bancário aprendizado.

--- SistemaBancario/main.swift
+++ SistemaBancario/main.swift
@@ -1,11 +1,42 @@
-//
-//  main.swift
-//  SistemaBancario
-//
-//  Created by Sara Regina Pires on 28/03/26.
-//
-
 import Foundation
 
-print("Hello, World!")
 
+struct ContaBancaria {
+    let numero: String
+    let titular: String
+    let senha: String
+    var saldo: Double
+    
+}
+
+//Dados de conta Adm
+let userAdmin = "adm01"
+let passUser = "01234"
+
+var contasBancarias: [ContaBancaria] = []
+var executarSessao: Bool = true
+
+print("Bem vindo ao sistema bancario - Projeto Curso Swift")
+
+while executarSessao {
+    print ("\n ========Menu de opções========")
+    print("1 - Criar conta")
+    print("2 - Area do Administrdor - listar as contas")
+    print("0 - Criar conta")
+    print("Escolha Uma das Opções: ", terminator: "")
+    
+    if let opcao = readLine() {
+        switch opcao {
+        case "1":
+            print("\n Nova Conta ")
+            print("Número:" )
+            
+        case "0":
+            executarSessao = false
+            print("Até logo!")
+
+        default:
+            print("Opção Invalida")
+        }
+    }
+}
