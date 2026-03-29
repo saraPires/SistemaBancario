1a9a418bfa1894a7df304b51708ada0d707d113c
sarapiresdev <sararegina.pires@gmail.com>
2026-03-29 01:04:52 -0300

    fazendo validação das contas e usuário logado

--- SistemaBancario/main.swift
+++ SistemaBancario/main.swift
@@ -21,15 +21,43 @@ print("Bem vindo ao sistema bancario - Projeto Curso Swift")
 while executarSessao {
     print ("\n ========Menu de opções========")
     print("1 - Criar conta")
-    print("2 - Area do Administrdor - listar as contas")
-    print("0 - Criar conta")
+    print("2 - Login Usuário (Acessar Minha Conta)")
+    print("3 - Area do Administrdor - listar as contas")
+    print("0 - Sair")
     print("Escolha Uma das Opções: ", terminator: "")
     
     if let opcao = readLine() {
         switch opcao {
         case "1":
-            print("\n Nova Conta ")
-            print("Número:" )
+            print("\n ======== Criar Conta ==========")
+            print("Número:", terminator: "" )
+            let num = readLine() ?? ""
+            print("Titular:", terminator: "")
+            let titular = readLine() ?? ""
+            print("Senha:", terminator: "")
+            let pass = readLine() ?? ""
+            print("Saldo Inicial: R$ ",terminator: "")
+            let saldoIni = Double(readLine() ?? "0") ?? 0.0
+            
+            
+            let conta = ContaBancaria(numero: num, titular: titular, senha: pass, saldo: saldoIni)
+            contasBancarias.append(conta)
+            print(" Conta de ", titular, "Criada!!")
+            
+        case "2":
+            print("\n ====== Login Cliente ===========")
+            print("Número da Conta: ", terminator: "")
+            let numContaBusca = readLine() ?? ""
+            print("Senha: ", terminator: "")
+            let senhaBusca = readLine() ?? ""
+            
+            if let i = contasBancarias.firstIndex(where: {$0.numero == numContaBusca && $0.senha == senhaBusca } ) {
+                var logado = true
+                
+            }
+            
+            
+            
             
         case "0":
             executarSessao = false
